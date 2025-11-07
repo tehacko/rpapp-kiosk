a1407bba1b18bd24433d06e61dcef7d812388f66
tehacko <tehacko@seznam.cz>
2025-11-07 21:20:29 +0100

    minor fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+//main Vanilla React Entry Point
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
